c47d46a7ea3f7c39e5a4e6ae2f1ecb2c1f102444
only1nglen <only1nglen@gmail.com>
2020-03-16 23:18:58 -0400

    Added projects and images

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Glen Nguyen.", "Developer.", "Food Seeker."],
       typeSpeed: 100,
       loop: true,
     });
